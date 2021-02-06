82790dede700d083d4ad83c63563cb63d7f071a8
Sota Yamaguchi <area029.0709@gmil.com>
2021-02-06 20:08:55 +0900

    fix: lint

--- pages/patient/index.tsx
+++ pages/patient/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Index = () => {
   return (
     <div className="container">
-      <h1>トップページ　患者</h1>
+      <h1>トップページ 患者</h1>
       <div style={{ height: '5rem' }}></div>
       <div>
         <Link href="/interview/list">
