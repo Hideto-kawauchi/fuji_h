cca4681108889ec1ff376cabbda575dd61a62eaf
s.saiki <bp18048@shibaura-it.ac.jp>
2021-02-07 11:49:41 +0900

    fix

--- pages/patient/index.tsx
+++ pages/patient/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 
+import { auth } from '@/lib/firebase';
+
 const Index = () => {
+  if (!auth.currentUser) return null
   return (
     <div className="container">
       <div className="object">マイページ ooさん</div>
       <div style={{ height: '5rem' }}></div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <Link href="/kihon">
+        <Link href={`/basePatient/${auth.currentUser?.uid}`}>
           <button>基本情報</button>
         </Link>
         <Link href="/patient/reserve">
